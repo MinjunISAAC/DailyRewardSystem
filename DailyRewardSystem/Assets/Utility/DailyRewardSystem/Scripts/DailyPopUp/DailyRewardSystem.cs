7f27ca8a11f3311a5ea597982cb6f9e8c25bb8b0
Min Jun <qkralswns931026@gmail.com>
2023-08-28 10:59:21 +0900

    [DEV] DailyRewardSystem 싱글톤 수정

--- DailyRewardSystem/Assets/Utility/DailyRewardSystem/Scripts/DailyPopUp/DailyRewardSystem.cs
+++ DailyRewardSystem/Assets/Utility/DailyRewardSystem/Scripts/DailyPopUp/DailyRewardSystem.cs
@@ -8,6 +8,7 @@ using Utility.ForAsset;
 using Utility.ForData.User;
 using InGame.DailySystem.ForUI;
 using InGame.DailySystem.ForData.Helper;
+using UnityEditor;
 
 namespace InGame.DailySystem.ForManage
 {
@@ -22,14 +23,35 @@ namespace InGame.DailySystem.ForManage
         // ----- Static Variables
         private static DailyRewardSystem _instance = null;
 
+        // ----- Variables
+        private const string FILE_PATH = "Assets/Utility/DailyRewardSystem/Prefabs/DailyRewardSystem.prefab";
+        private bool _isSingleton = false;
+
         // ----- Property
         public static DailyRewardSystem Instance
         {
             get
             {
-                if (null == _instance) 
+                if (null == _instance)
+                {
+                    var existingInstance = FindObjectOfType<DailyRewardSystem>();
+
+                    if (existingInstance != null)
+                    {
+                        _instance = existingInstance;
+                    }
+                    else
+                    {
+                        var origin = AssetDatabase.LoadAssetAtPath<DailyRewardSystem>(FILE_PATH);
+                        _instance = Instantiate<DailyRewardSystem>(origin);
+                        _instance._isSingleton = true;
+                        DontDestroyOnLoad(_instance.gameObject);
+                    }
+                }
+
+                if (null == _instance)
                     _instance = new DailyRewardSystem();
-                
+
                 return _instance;
             }
         }
@@ -38,19 +60,28 @@ namespace InGame.DailySystem.ForManage
         // Components
         // --------------------------------------------------
         [SerializeField] private DailyPopUpView _dailyPopUpView = null;
-        [SerializeField] private Button         _BTN_dailyPopUp = null;
+        [SerializeField] private Button _BTN_dailyPopUp = null;
 
         // --------------------------------------------------
         // Variables
         // --------------------------------------------------
-        private DailyRewardDataHelper _dailyRewardDataHelper = null; 
-        
+        private DailyRewardDataHelper _dailyRewardDataHelper = null;
+
         // --------------------------------------------------
         // Functions - Event
         // --------------------------------------------------
-        private void Awake() 
-        { 
-           DontDestroyOnLoad(this);
+        private void Awake()
+        {
+            if (null == _instance)
+            {
+                var existingInstance = FindObjectOfType<DailyRewardSystem>();
+
+                if (existingInstance != null)
+                {
+                    _instance = existingInstance;
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+            }
 
             if (_dailyRewardDataHelper == null)
                 _dailyRewardDataHelper = new DailyRewardDataHelper();
@@ -62,23 +93,23 @@ namespace InGame.DailySystem.ForManage
         // --------------------------------------------------
         // Functions - Nomal
         // --------------------------------------------------
-        public void OnInit() 
-        { 
+        public void OnInit()
+        {
             var datas = JsonParser.GetDataSet();
-            
+
             _dailyPopUpView.OnInit();
             _dailyPopUpView.SetToRewardItems(datas, _Reward);
         }
 
-        public void VisiableDailyPopUp(bool isShow)    => _dailyPopUpView.gameObject.SetActive(isShow);
+        public void VisiableDailyPopUp(bool isShow) => _dailyPopUpView.gameObject.SetActive(isShow);
         public void VisiableDailyPopUpBtn(bool isShow) => _BTN_dailyPopUp.gameObject.SetActive(isShow);
-        private void _Reward(EAssetType assetType, int cost) 
+        private void _Reward(EAssetType assetType, int cost)
         {
-            switch (assetType) 
+            switch (assetType)
             {
                 case EAssetType.Coin: UserSaveDataManager.AddCoin(cost); break;
-                case EAssetType.Gem:  UserSaveDataManager.AddGem (cost); break;
+                case EAssetType.Gem: UserSaveDataManager.AddGem(cost); break;
             }
         }
     }
-} 
+}
